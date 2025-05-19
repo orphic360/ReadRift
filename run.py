2e61c313a431dfcaecb7919feaa81e2e7581e01f
Laiba Ishtiaq <laibaishtiaq360@gmail.com>
2025-05-20 00:40:36 +0500

    Initial commit

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensures the database tables are created
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
